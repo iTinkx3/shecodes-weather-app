c0a90dabcf1b15a65f1e71e0246db4744e8c8592
iTinkx3 <132921741+iTinkx3@users.noreply.github.com>
2023-07-20 22:27:31 +0100

    Showing the current temperature

---
+++ src/app.js
@@ -0,0 +1,21 @@
+function displayTemperature(response) {
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#current-temp");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind")
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.main.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+
+
+let apiKey = "d0d69cd1107bdb285d6980ec9a9c4f97";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+
+axios.get(apiUrl).then(displayTemperature);
