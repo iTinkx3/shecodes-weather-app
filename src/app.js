1d0869122cf27c032639f7f5b89d5c68a232a9cf
iTinkx3 <132921741+iTinkx3@users.noreply.github.com>
2023-08-09 19:57:14 +0100

    Added forecast to javascript

--- src/app.js
+++ src/app.js
@@ -16,9 +16,28 @@ let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "S
 
 function displayForecast() {
     let forecastElement = document.querySelector("#forecast");
-    let forecastHTML = "";
-    forecastElement.innerHTML = forecastHTML;
+
+    let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+
+    let forecastHTML = `<div class="row">`;
+    days.forEach(function(day) {
+      forecastHTML = forecastHTML + 
+        `
+            <div class="col-2">
+              <div class="weather-forecast-date">
+               ${day} 
+              </div>
+              <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="" width="36">
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max">18°</span> 
+                <span class="weather-forecast-temperature-min">12°</span>
+              </div>
+            </div>
+          `;  
+    })
     
+          forecastHTML = forecastHTML + `</div>`;
+          forecastElement.innerHTML = forecastHTML;
 }
 
 function displayTemperature(response) {
@@ -30,6 +49,7 @@ function displayTemperature(response) {
     let windElement = document.querySelector("#wind")
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
+
 
     celsiusTemperature = response.data.main.temp;
 
